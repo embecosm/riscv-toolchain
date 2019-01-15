b41ef89c8a411c3b96ab063923f4b08d44e13b3a
Graham Markall <graham.markall@embecosm.com>
2019-01-15 00:42:44 +0000

    Script for benchmarking using BEEBS
    The script is invoked with:
    
      ./benchmark-beebs.py
    
    and can be run after the RISC-V and ARM toolchains are built.
    
    Files Changed:
    
            * benchmark-beebs.py: New file.

---
+++ benchmark-beebs.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+import datetime
+import itertools
+import logging
+import os
+import shutil
+import subprocess
+import sys
+
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+TOP_DIR = os.path.abspath(os.path.join(CURRENT_DIR, '..'))
+BEEBS_DIR = os.path.join(TOP_DIR, 'beebs')
+
+HOSTS = { 'riscv': 'riscv32-unknown-elf', 'arm': 'arm-none-eabi' }
+
+CONFIGS = {
+ 'baseline':
+     '--with-chip=compare-baseline --with-board=generic',
+ 'nocrt':
+     '--with-chip=compare-nocrt --with-board=generic --enable-dummy-crt0',
+ 'nolibc':
+     '--with-chip=compare-nolibc --with-board=generic --enable-dummy-crt0 --enable-dummy-libc',
+ 'nolibc-nolibgcc':
+     '--with-chip=compare-nolibc-nolibgcc --with-board=generic --enable-dummy-crt0 --enable-dummy-libc --enable-dummy-libgcc',
+ 'nolibc-nolibgcc-nolibm':
+     '--with-chip=compare-nolibc-nolibgcc --with-board=generic --enable-dummy-crt0 --enable-dummy-libc --enable-dummy-libgcc --enable-dummy-libm'
+}
+
+
+log = logging.getLogger()
+
+def setup_logging(logfile):
+    log.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.INFO)
+    log.addHandler(ch)
+    fh = logging.FileHandler(logfile)
+    fh.setLevel(logging.DEBUG)
+    log.addHandler(fh)
+
+class RunCommandError(RuntimeError):
+    '''Exception raised when something went wrong with execution of command.'''
+    pass
+
+def run_command(args, *, timeout, work_dir, toolchain_dir):
+    log.debug('Running %s' % " ".join(args))
+    path = toolchain_dir + os.pathsep + os.environ['PATH']
+    env = { 'PATH': path }
+    try:
+        cp = subprocess.run(args, capture_output=True, check=True, timeout=timeout, cwd=work_dir, env=env)
+        log.info('Process exited normally')
+        log.debug('Stdout:\n\n%s\n\n' % cp.stdout.decode())
+        log.debug('Stderr:\n\n%s\n\n' % cp.stderr.decode())
+        return
+    except subprocess.TimeoutExpired as e:
+        log.info('Execution timeout (%ss) expired') % timeout
+        log.info('Stdout:\n\n%s\n\n' % e.stdout.decode())
+        log.info('Stderr:\n\n%s\n\n' % e.stderr.decode())
+        raise RunCommandError
+    except subprocess.CalledProcessError as e:
+        log.info('Process exited abnormally with code %s' % e.returncode)
+        log.info('Stdout:\n\n%s\n\n' % e.stdout.decode())
+        log.info('Stderr:\n\n%s\n\n' % e.stderr.decode())
+        raise RunCommandError
+
+def build(host, config):
+    build_dir = os.path.join(TOP_DIR, 'build-%s' % host, 'beebs-%s' % config)
+    log.info('Building in %s' % build_dir)
+    if os.path.exists(build_dir):
+        if os.path.isdir(build_dir):
+            shutil.rmtree(build_dir)
+        else:
+            os.remove(build_dir)
+    try:
+        os.mkdir(build_dir)
+    except FileExistsError:
+        log.info('Using existing build dir')
+    configure = os.path.join(BEEBS_DIR, 'configure')
+    host_arg = '--host=%s' % HOSTS[host]
+    toolchain_dir = os.path.join(TOP_DIR, 'install-%s' % host, 'bin')
+    try:
+        log.info('Configuring...')
+        config_args = [ configure, host_arg ] + CONFIGS[config].split() + [ '--disable-maintainer-mode' ]
+        run_command(config_args, timeout=30, work_dir=build_dir, toolchain_dir=toolchain_dir)
+        log.info('Building...')
+        run_command(['make', '-j9'], timeout=120, work_dir=build_dir, toolchain_dir=toolchain_dir)
+        log.info('Benchmarking...')
+        run_command(['make', 'check'], timeout=60, work_dir=build_dir, toolchain_dir=toolchain_dir)
+    except RunCommandError:
+        log.info('Aborting due to error in command execution.')
+        raise
+
+def main():
+    log_name = 'benchmark-beebs-%s.log' % datetime.datetime.now().strftime('%Y-%m-%d-%H%M')
+    log_path = os.path.join(TOP_DIR, 'logs', log_name)
+    setup_logging(log_path)
+
+    log.info('Top dir: %s', TOP_DIR)
+    for host, config in itertools.product(HOSTS, CONFIGS):
+        log.info('\nBuilding %s on %s' % (config, host))
+        try:
+            build(host, config)
+        except RunCommandError:
+            return 1
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
